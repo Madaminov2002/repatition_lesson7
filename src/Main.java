45c526e3bb9d459f0b5f79cdd858ff1ee8327031
Suhrb <suhrobjonmadaminov37@gmail.com>
2023-10-29 11:59:37 +0500

    exam 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.io.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-//eeeeegit
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectOutputStream os=new ObjectOutputStream(new FileOutputStream("recourse/exam1.text"));
+        os.writeObject("Salom");
+        os.close();
+
+        ObjectInputStream ois=new ObjectInputStream(new FileInputStream("recourse/exam1.text"));
+        Object o = ois.readObject();
+        System.out.println("o = " + o);
+        ois.close();
 
     }
 }
